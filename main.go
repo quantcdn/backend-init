94302f8c0a9c719025d360a6ffca7bd2c0880b0a
Steve Worley <sj.worley88@gmail.com>
2023-08-18 14:17:40 +1000

    Add lock and skip-first.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/alecthomas/kong"
@@ -9,15 +10,28 @@ import (
 )
 
 var cli struct {
-	Verify commands.Verify `cmd:"" help:"Verify a backend HTTP connection to the given URL."`
+	Verify    commands.Verify `cmd:"" help:"Verify a backend HTTP connection to the given URL."`
+	SkipFirst bool            `help:"First run always returns success" default:"false" short:"s"`
+	Lock      string          `help:"Lock file location" default:"/data"`
 }
 
 func main() {
 	ctx := kong.Parse(&cli)
-	err := ctx.Run()
+	lock := fmt.Sprintf("%s/init.lock", cli.Lock)
+	_, err := os.Stat(lock)
+
+	if cli.SkipFirst && err != nil {
+		os.Create(lock)
+		log.Info().Msg("Skipping first run,")
+		os.Exit(0)
+	}
+
+	err = ctx.Run()
+
 	if err != nil {
 		log.Error().AnErr("error", err)
 		os.Exit(1)
 	}
+
 	os.Exit(0)
 }
